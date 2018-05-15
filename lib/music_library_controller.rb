892adcf6500270bf86bb3fd3ff5bf7a83a8249b1
Catherine Salazar <catcreates13@gmail.com>
2018-05-15 02:33:24 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -54,4 +54,16 @@ def list_songs_by_artist
 end
 end
 
+def list_songs_by_genre
+  puts "Please enter the name of an artist:"
+  input = gets.strip
+
+  if artist = Artist.find_by_name(input)
+    artist.songs.sort_by(&:name).each.with_index(1) do |song, index|
+      puts "#{index}. #{song.name} - #{song.genre.name}"
+    end
+end
+end
+
+
 end
