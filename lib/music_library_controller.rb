0866cb03e5a79e3bf9a13ad0df369b7959326b56
Catherine Salazar <catcreates13@gmail.com>
2018-05-15 02:12:32 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -32,6 +32,7 @@ class MusicLibraryController
   end
 
   def list_artists
- binding.pry
+    Artist.all.sort_by(&:name).each.with_index(1) do |song, index|
+      puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
   end
 end
