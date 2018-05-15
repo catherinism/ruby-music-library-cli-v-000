022b573d0ccf03b7f0b77e19e7a838e08e68e3c9
Catherine Salazar <catcreates13@gmail.com>
2018-05-15 02:33:40 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -55,7 +55,7 @@ end
 end
 
 def list_songs_by_genre
-  puts "Please enter the name of an artist:"
+  puts "Please enter the name of a genre:"
   input = gets.strip
 
   if artist = Artist.find_by_name(input)
