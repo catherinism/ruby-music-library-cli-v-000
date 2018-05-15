9f33a8b281d581ce7dfc2f3930a7bdbe71bb429f
Catherine Salazar <catcreates13@gmail.com>
2018-05-15 03:00:18 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -72,7 +72,6 @@ def play_song
       if (1..Song.all.length).include?(input)
         song = Song.all.sort_by(&:name)[input - 1]
       end
-
       puts "Playing #{song.name} by #{song.artist.name}" if song
     end
   end
